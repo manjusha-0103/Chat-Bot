d07e8e3eb20e5b347f837217ff21124c94b7720a
Manjusha <93437320+manjusha-0103@users.noreply.github.com>
2023-11-08 11:48:21 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(bodyparser.urlencoded({extended:true}));
 //     origin: 'http://192.168.0.106:19000',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
-
+//  MONGO_URI = "YOUR_MONGO_URI"
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser : true,
     useUnifiedTopology :true
